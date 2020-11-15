d8bc5b33e33ca140ec81f29e49a38876cd0e0c9e
Tianyi Zhang <zhangti@scss.tcd.ie>
2020-11-15 21:44:04 +0000

    run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+if [ ! -d "venv/" ]; then
+	python3 -m venv --system-site-packages ./venv
+fi
+source venv/bin/activate
+if [ -d "/users/ugrad/zhangti/project2/OpenBLAS/lib/" ]; then
+	export LD_LIBRARY_PATH=/users/ugrad/zhangti/project2/OpenBLAS/lib/
+fi
+pip3 install numpy
+pip3 install opencv-python
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
+git pull
+python3 classify.py --model-name model --captcha-dir ./captchas/ --output ./stuff.txt --symbols symbols.txt
+git add ./stuff.txt
+git commit -m "classifed captchas"
+git push
