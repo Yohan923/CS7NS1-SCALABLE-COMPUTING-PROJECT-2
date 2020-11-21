5497a8d71f04c647b4c395441793ffc067fe380b
Tianyi Zhang <zhangti@scss.tcd.ie>
2020-11-21 20:25:54 +0000

    mod script

--- run.sh
+++ run.sh
@@ -10,6 +10,3 @@ pip3 install opencv-python
 pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
 git pull
 python3 classify.py --model-name model --captcha-dir ./captchas/ --output ./stuff.txt --symbols symbols.txt
-git add ./stuff.txt
-git commit -m "classifed captchas"
-git push
